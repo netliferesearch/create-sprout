23e20de7512f00419caf9f2f2aaef6710994a1dd
Haakon Borch <haakon.borch@netlife.com>
2020-06-05 15:37:53 +0200

    :construction: add js-object containing string to replace and fallbacks

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@
 import arg from "arg";
 import inquirer from "inquirer";
 import { createSprout } from "./main";
+import defaults from "./defaults";
 
 // TODO: This should preferably come from a config, with sensible default values
 const defaultTemplate = "Fullstack Gatsby Sanity";
@@ -16,17 +17,17 @@ function parseArgumentsIntoOptions(rawArgs) {
       "--install": Boolean,
       "-g": "--git",
       "-y": "--yes",
-      "-i": "--install",
+      "-i": "--install"
     },
     {
-      argv: rawArgs.slice(2),
+      argv: rawArgs.slice(2)
     }
   );
   return {
     skipPrompts: args["--yes"] || false,
     git: args["--git"] || false,
     template: args._[0],
-    runInstall: args["--install"] || false,
+    runInstall: args["--install"] || false
   };
 }
 
@@ -35,7 +36,7 @@ async function prompForMissingOptions(options) {
   if (options.skipPrompts) {
     return {
       ...options,
-      template: options.template || defaultTemplate,
+      template: options.template || defaultTemplate
     };
   }
   // Define prompt questions
@@ -46,7 +47,7 @@ async function prompForMissingOptions(options) {
       name: "template",
       message: "Please choose which project template to use",
       choices: ["Fullstack Gatsby Sanity", "Fullstack Next Sanity"],
-      default: defaultTemplate,
+      default: defaultTemplate
     });
   }
   if (!options.git) {
@@ -54,12 +55,16 @@ async function prompForMissingOptions(options) {
       type: "confirm",
       name: "git",
       message: "Initialize a git repository",
-      default: false,
+      default: false
     });
   }
 
   // TODO: These strings should be set through the CLI, with gracious fallbacks
-
+  questions.push({
+    type: "input",
+    name: "defaults",
+    message: "Get default-values for each value to be replaced"
+  });
   // gatsby active environment - develop
   // node version - 12.14.0
   // owners name - Client Inc.
@@ -79,7 +84,7 @@ async function prompForMissingOptions(options) {
   return {
     ...options,
     template: options.template || answers.template,
-    git: options.git || answers.git,
+    git: options.git || answers.git
   };
 }
 
---
+++ src/defaults.js
@@ -0,0 +1,31 @@
+export default {
+  "gatsby-environment": {
+    "<% replace with gatsby active environment %>": "develop"
+  },
+  "node-version": { "<% replace with node version %>": "12.14.0" },
+  "owners-name": { "<% replace with owners name %>": "Client Inc." },
+  "": { "<% replace with project description %>": "Client Inc. is blah blah" },
+  "project-name": { "<% replace with project name %>": "The Project Name" },
+  "project-kebab-case": {
+    "<% replace with project name kebab-case %>": "the-project-name"
+  },
+  "repo-base-url": {
+    "<% replace with project repo base url %>": "user/the-project-name"
+  },
+  "repo-full-url": {
+    "<% replace with project repo full url %>":
+      "https://github.com/user/the-project-name"
+  },
+  "sanity-auth-token": {
+    "<% replace with sanity token %>":
+      "a lengthy hash string that allows making requests to Sanity Studio through the frontend"
+  },
+  "sanity-project-id": {
+    "<% replace with sanity project id %>":
+      "a short hash string that connects the local Sanity Studio to the remote one"
+  },
+  "sanity-dataset": {
+    "<% replace with sanity current dataset %>": "production"
+  },
+  "site-url": { "<% replace with site url %>": "https://the-project-name.com" }
+};
